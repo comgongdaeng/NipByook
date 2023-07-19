a6241243ba968a480b3a998349149b113004722f
Gamddalki <hannahk01@naver.com>
2023-07-19 12:53:28 +0200

    Create Scene Controller

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,27 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void GoLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void GoLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void GoLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void GoGameClear()
+    {
+        SceneManager.LoadScene("GameClear");
+    }
+}
